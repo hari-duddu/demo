bd38c47ddfec48e36b4b56c37f944360de0b4bee
Hari Duddu <hduddu@mapr.com>
2022-05-30 17:30:47 +0530

    Added one more print2

--- Test1.java
+++ Test1.java
@@ -1,8 +1,7 @@
 Class Test1{
   public static void main(String args[]){
         System.out.println("Hi");
-        System.out.println("Hi2");
-        System.out.println("Hi3");
-
+        System.out.println("Hi");
+        System.out.println("How are you");
   }
 }
